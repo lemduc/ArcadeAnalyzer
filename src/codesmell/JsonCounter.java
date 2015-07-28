2fa6f950e8303b49dcf3c5ccaae8aa1209ccd053
Duc M. Le <duclm.bk@gmail.com>
2015-07-27 22:22:07 -0700

    New Analyzer

---
+++ src/codesmell/JsonCounter.java
@@ -0,0 +1,20 @@
+package codesmell;
+
+import java.io.File;
+import java.io.IOException;
+
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.core.JsonParser;
+
+public class JsonCounter {
+	public static void main(String[] args) throws JsonParseException, IOException{
+		String testInput = "F:\\code_smell\\output-apache-activemq\\apache-activemq- (tag_ activemq-4.0)-anomalies.json";
+		File file = new File(testInput);
+		
+		JsonFactory jsonFactory = new JsonFactory(); // or, for data binding, org.codehaus.jackson.mapper.MappingJsonFactory 
+		JsonParser jp = jsonFactory.createJsonParser(file); // or URL, Stream, Reader, String, byte[]
+		
+		jp.nextToken();
+	}
+}
